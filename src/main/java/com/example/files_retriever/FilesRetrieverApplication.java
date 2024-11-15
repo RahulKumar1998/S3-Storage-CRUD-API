647fe2c5233f551e5b390ae1d252e813cd4c85ec
RahulKumar1998 <rahulkumar08.work@gmail.com>
2024-11-15 13:05:19 +0530

    basic project structure setup

---
+++ src/main/java/com/example/files_retriever/FilesRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.example.files_retriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesRetrieverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/files_retriever/FilesRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.files_retriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
